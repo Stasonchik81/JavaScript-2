14704bae5a736c79051bfec718a99a8d92dd98e9
stasonchik1101 <stasonchik1101@gmail.com>
2021-01-27 22:52:40 +0300

    начало игры Морской бой

--- Project_03/code.js
+++ Project_03/code.js
@@ -1,4 +1,5 @@
-function makeGetRequest(url, callback){
+// Получение данных с помощью calback
+/*function makeGetRequest(url, callback){
     var xhr;
     if (window.XMLHttpRequest){
         xhr = new XMLHttpRequest();
@@ -13,18 +14,61 @@ function makeGetRequest(url, callback){
     xhr.open('GET', url, true);
     xhr.send();
 }
-  
+*/
+ // Получение данных с помощью promise
+function makeGetRequest(url){
+    return new Promise((resolve) => {
+        var xhr;
+        if (window.XMLHttpRequest){
+        xhr = new XMLHttpRequest();
+        } else if (window.ActiveXObject){
+        xhr = new ActiveXObject("Microsoft.XMLHTTP");
+            }
+        xhr.onreadystatechange = function () {
+            if(xhr.readyState === 4){
+            resolve(xhr.responseText);
+            }
+        }
+        xhr.open('GET', url, true);
+        xhr.send();
+    })   
+}
+
+// function makeGetRequest(url){
+//     return new Promise(function(resolve, reject){
+//         var xhr;
+//         if (window.XMLHttpRequest){
+//         xhr = new XMLHttpRequest();
+//         } else if (window.ActiveXObject){
+//         xhr = new ActiveXObject("Microsoft.XMLHTTP");
+//             }
+//         xhr.open('GET', url, true);
+//         xhr.onload = () => resolve(xhr.responseText);
+//         xhr.onerror = () => reject(console.error('Ошибка загрузки скрипта'));
+//         xhr.open('GET', url, true);
+//         xhr.send();
+//     })
+// }
+
 class GoodList{
     constructor(){
         this.goods = [];
     }
-    fetchGoods(cb){
+    fetchGoods(){
         const API_URL = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses';
-        makeGetRequest(`${API_URL}/catalogData.json`, (goods) => {
-            this.goods = JSON.parse(goods);
-            cb();
+        makeGetRequest(`${API_URL}/catalogData.json`)
+            .then((data) => {this.goods = JSON.parse(data);
+            //this.render();
         });
     }
+    // render(){
+    //     let list = '';
+    //     this.goods.forEach(el => {
+    //         const item = new GoodsItem(el.product_name, el.price);
+    //         list += item.render();
+    //     })
+    //     document.querySelector('.goods-list').innerHTML = list;
+    // }
     render(){
         let list = '';
         this.goods.forEach(el => {
@@ -76,6 +120,4 @@ class CartItem{
     }
 }
 const List = new GoodList();
-List.fetchGoods(() => {List.render()});
-
-console.log(List.totalCost());
+List.fetchGoods();
